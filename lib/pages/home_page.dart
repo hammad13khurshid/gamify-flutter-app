b11fa95e8765f9dc6c9adec205251d9cdd894f8f
hammad13khurshid <hammadkhurshid931@gmail.com>
2024-04-22 23:39:43 +0500

    Added some new code for the image sliders on home screen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../data.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,10 +11,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  var _deviceHeight;
+  var _deviceWidth;
+
+  @override
+  void initState(){
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+
     return Scaffold(
-      body: Container(),
+      body: Stack(children: <widget>[],),
     );
   }
+
+  Widget _featuredGamesWidget{}
+{
+  return SizedBox(
+  height: _deviceHeight * 0.50,
+  width: _deviceWidth ,
+  child: Container(
+   decoration: BoxDecoration(image: DecorationImage ),) ,);
+}
 }
